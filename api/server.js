db7143168c11d1a8bced38d829796bb1006f5419
Lukecp <lukecp@pm.me>
2021-03-19 23:01:57 -0400

    MVP completed

--- api/server.js
+++ api/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const server = express();
 
+const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
+
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
+
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
